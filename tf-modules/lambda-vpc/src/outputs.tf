3e414d96a0bcf0e1a37f1cf03339081c2cd77af0
vuvuzella <j.tabacii@gmail.com>
2022-09-11 11:51:43 +1000

    added outputs to the lambda-vpc module

--- tf-modules/lambda-vpc/src/outputs.tf
+++ tf-modules/lambda-vpc/src/outputs.tf
@@ -1,3 +1,9 @@
-output "hello" {
-  value = "Hello World"
+output "arn" {
+  description = "ARN identifying the lambda function"
+  value = aws_lambda_function.lambda.arn
+}
+
+output "invoke_arn" {
+  description = "ARN to be used for invoking the lambda function from an API gateway. To be used in aws_api_gateway_integration"
+  value = aws_lambda_function.lambda.invoke_arn
 }
